5489e4a5850cacf85e46791942b4a0ddd17b7487
RodrigoPatrao <rodrigo.viveiros@outlook.com>
2017-12-28 20:56:22 -0200

    Ad model generated, relations done

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ group :development, :test do
 end
 
 group :development do
+  # Generate Entity-Relationship Diagrams for Rails applications
+  gem 'rails-erd', require: false
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228213501) do
+ActiveRecord::Schema.define(version: 20171228220219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 20171228213501) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+
+  create_table "ads", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description"
+    t.integer  "category_id"
+    t.integer  "member_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "ads", ["category_id"], name: "index_ads_on_category_id", using: :btree
+  add_index "ads", ["member_id"], name: "index_ads_on_member_id", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "description", limit: 60
@@ -58,4 +70,6 @@ ActiveRecord::Schema.define(version: 20171228213501) do
   add_index "members", ["email"], name: "index_members_on_email", unique: true, using: :btree
   add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "ads", "categories"
+  add_foreign_key "ads", "members"
 end
