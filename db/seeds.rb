8f55a8f92d2e0c279446ef605378038b2d660be2
RodrigoPatrao <rodrigo.viveiros@outlook.com>
2018-01-02 00:09:58 -0200

    Enum for access type (role)

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,7 @@ Admin.create!(
   name: 'Administrador Geral',
   email: 'admin@admin.com',
   password: '123456',
-  password_confirmation: '123456'
+  password_confirmation: '123456',
+  role: 0
 )
 puts 'Default Admin created.'
