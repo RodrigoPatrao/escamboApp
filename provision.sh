fd6945b386428f3aaf76dc9911649f61a11ecaad
RodrigoPatrao <rodrigo.viveiros@outlook.com>
2018-01-24 14:54:54 -0200

    Vagrant on Win10 setup complete - app running

--- provision.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update
-sudo apt-get remove ruby -y
-curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
-curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0
-source /etc/profile.d/rvm.sh
-sudo apt-get install postgresql postgresql-contrib libpq-dev -y
